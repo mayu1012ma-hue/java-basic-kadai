1dd6d0d842c44adf3dad25fe050412be0e05ec7c
mayu1012ma-hue <mayu.1012.ma@gmail.com>
2026-09-24 02:02:35 +0900

    課題18
    課題ファイルを追加

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+//加藤花子を表すクラス
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	
+	public void setGivenName() {
+		givenName = "花子";
+	}
+	
+	void eachIntroduce() {
+		System.out.println("趣味は読書です");
+	}
+		
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+
+//加藤太郎を表すクラス
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+
+	
+	public void setGivenName(){
+		givenName = "太郎";
+	}
+	
+	void eachIntroduce() {
+		System.out.println("Javaが得意です");
+	}
+
+	
+}
